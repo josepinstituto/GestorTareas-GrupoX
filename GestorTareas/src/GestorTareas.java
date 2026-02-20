3754a6f6e94df76125f50305124cf312462bd261
juan <juanillogarcia14@gmail.com>
2026-02-20 13:03:48 +0100

    Creado metodo añadirTarea

--- GestorTareas/src/GestorTareas.java
+++ GestorTareas/src/GestorTareas.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class GestorTareas {
+	
+	private ArrayList<Tarea> listaTareas;
 
+	/**
+	*Metodo para añadir tarea
+	*@param nombre de la tarea
+	*/
+	private void addTarea(String nombreTarea) {
+		listaTareas.add(new Tarea(nombreTarea));
+	}
 }
