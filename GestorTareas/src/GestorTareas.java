7cd20c6f363f6e58a100f2a60607256cf234a77f
juan <juanillogarcia14@gmail.com>
2026-02-20 13:15:37 +0100

    Creado metodo completarTarea

--- GestorTareas/src/GestorTareas.java
+++ GestorTareas/src/GestorTareas.java
@@ -8,7 +8,12 @@ public class GestorTareas {
 	*Metodo para añadir tarea
 	*@param nombre de la tarea
 	*/
-	private void addTarea(String nombreTarea) {
-		listaTareas.add(new Tarea(nombreTarea));
+	private void addTarea(String descripcion) {
+		listaTareas.add(new Tarea(descripcion));
+	}
+	
+	private void completarTarea(int indice){
+		listaTareas.get(indice).setCompletada(true);
+		System.out.println("Tarea completada");
 	}
 }
